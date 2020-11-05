1019524d173b454f885c64de123f732074874cb0
Jacob Cowan <mrjcowman@gmail.com>
2020-11-05 13:14:43 -0800

    Add friend counter

--- src/components/pages/Discover.js
+++ src/components/pages/Discover.js
@@ -2,14 +2,23 @@ import React from "react";
 import PupCard from "../PupCard.js"
 
 class Discover extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {friends: 0};
+  }
+
+  addFriend = () => {
+    this.setState({friends: this.state.friends+1})
+  }
+
   render() {
     return (
       <div className="text-center row justify-content-center">
         <div className="col-sm-12 col-md-6 col-lg-4">
           <h1>Make New Friends</h1>
           <p>Thumbs up on any pup you'd like to meet!</p>
-          <PupCard />
-          <p>Friends made so far: 0</p>
+          <PupCard addFriend={this.addFriend}/>
+          <p>Friends made so far: {this.state.friends}</p>
         </div>
       </div>
     )
